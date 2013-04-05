c5ee889bcc8382a4a094caf74253bbbd42e19e38
Kristoffer Bergkvist <kristofferbergkvist@Kristoffers-MacBook-Pro.local>
2013-04-05 11:06:21 +0200

    Initial Commit

---
+++ demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo1
+//
+//  Created by Kristoffer Bergkvist on 2013-04-05.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
