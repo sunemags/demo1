4c52cb387aae66d26cf53456b3f4cc95a1f9e9ed
Kristoffer Bergkvist <kristofferbergkvist@Kristoffers-MacBook-Pro.local>
2013-04-05 11:18:35 +0200

    Added DemoFileX

---
+++ demo1/DemoFileX.m
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.m
+//  demo1
+//
+//  Created by Kristoffer Bergkvist on 2013-04-05.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import "DemoFileX.h"
+
+@implementation DemoFileX
+
+@end
