b46ea76c63e117028609fbff7d819e44293f763f
Kristoffer Bergkvist <kristofferbergkvist@Kristoffers-MacBook-Pro.local>
2013-04-05 11:08:37 +0200

    Added project

---
+++ demo1/DemoFile.h
@@ -0,0 +1,13 @@
+//
+//  DemoFile.h
+//  demo1
+//
+//  Created by Kristoffer Bergkvist on 2013-04-05.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFile : NSObject
+
+@end
---
+++ demo1/DemoFile.m
@@ -0,0 +1,13 @@
+//
+//  DemoFile.m
+//  demo1
+//
+//  Created by Kristoffer Bergkvist on 2013-04-05.
+//  Copyright (c) 2013 Kristoffer Bergkvist. All rights reserved.
+//
+
+#import "DemoFile.h"
+
+@implementation DemoFile
+
+@end
